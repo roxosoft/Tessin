a68f4d68b9b29a9701f8545da9d441383f670777
Philip Mantrov <philip.mantrov@gmail.com>
2018-08-09 14:42:26 +0300

    second version with static url

--- Html5Banners/V1/250x360/gulpfile.js
+++ Html5Banners/V1/250x360/gulpfile.js
@@ -17,6 +17,7 @@ var merge        = require('merge-stream');
 gulp.task('default', function() {
 
   gulp.run(['compile-banner']);
+  gulp.run(['compile-banner-static']);
 
   browserSync.init({
     server: "./dist/test/"
@@ -31,6 +32,7 @@ gulp.task('default', function() {
 gulp.task('compile-img', function(){
    gulp.src('./src/img/*')
     .pipe(gulp.dest('./dist/test/'))
+    .pipe(gulp.dest('./dist/prod-static/'))
     .pipe(gulp.dest('./dist/prod/'));
 });
 
@@ -92,16 +94,39 @@ gulp.task('compile-html-prod', ['compile-styl', 'compile-img', 'compile-fonts'],
     .pipe(gulp.dest('./dist/prod/'));
 });
 
+gulp.task('compile-html-prod-static', ['compile-styl', 'compile-img', 'compile-fonts'], function(){
+    return gulp.src('./src/templates/index.html')
+        .pipe(injectCSS())
+        .pipe(minify({
+            minify: true,
+            minifyJS: true,
+            uglifyJS: true,
+            uglifyCSS: true,
+            minifyCSS: true,
+            minifyHTML: true,
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest('./dist/prod-static/'));
+});
+
 gulp.task('compile-banner', ['compile-html', 'compile-html-prod'], function(){
 
   return gulp.src(['./dist/prod/index.html', './dist/prod/image.jpg', './dist/prod/logo.svg', './dist/prod/stroke.svg'])
     .pipe(zip('Banner_V1_250x360.zip'))
-    .pipe(gulp.dest('./dist/prod/'));
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('compile-banner-static', ['compile-html-prod-static'], function(){
+
+    return gulp.src(['./dist/prod-static/index.html', './dist/prod-static/image.jpg', './dist/prod-static/logo.svg', './dist/prod-static/stroke.svg'])
+        .pipe(zip('Banner_V1_250x360-static.zip'))
+        .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('compile-js', function(){
   return gulp.src('./src/js/*.js')
     .pipe(gulpBrowser.browserify())
+    .pipe(gulp.dest('./dist/prod-static/js/'))
     .pipe(gulp.dest('./dist/prod/js/'))
     .pipe(gulp.dest('./dist/test/js/'));
 });
